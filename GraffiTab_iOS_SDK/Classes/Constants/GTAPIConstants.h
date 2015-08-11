8432fe6ad07e61af1642a41acde38e68c5c9d156
Georgi Christov <georgi.christov89@gmail.com>
2015-08-11 16:59:02 +0100

    no message

--- GraffiTab_iOS_SDK/Classes/Constants/GTAPIConstants.h
+++ GraffiTab_iOS_SDK/Classes/Constants/GTAPIConstants.h
@@ -9,9 +9,9 @@
 #ifndef TheBestOfBulgaria_iOS_APIConstants_h
 #define TheBestOfBulgaria_iOS_APIConstants_h
 
-#define API_APP_URL @"http://localhost:8080/TriaServerTomcat"
+//#define API_APP_URL @"http://localhost:8080/TriaServerTomcat"
 //#define API_APP_URL @"http://192.168.0.8:8080/TriaServerTomcat"
-//#define API_APP_URL @"http://graffitab.com"
+#define API_APP_URL @"http://graffitab.com/TriaServerTomcat"
 #define API_APP_API_URL @"api/rest"
 
 #define API_USER_REGISTER                         @"/user/register"
