3e496be31d53f728007f8af73526df893aeec49a
Georgi Christov <georgi.christov89@gmail.com>
2016-04-04 14:28:43 +0100

    no message

---
+++ GraffiTab-iOS-SDK/GraffiTab-iOS-SDK.h
@@ -0,0 +1,19 @@
+//
+//  GraffiTab-iOS-SDK.h
+//  GraffiTab-iOS-SDK
+//
+//  Created by Georgi Christov on 04/04/2016.
+//  Copyright © 2016 GraffiTab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraffiTab-iOS-SDK.
+FOUNDATION_EXPORT double GraffiTab_iOS_SDKVersionNumber;
+
+//! Project version string for GraffiTab-iOS-SDK.
+FOUNDATION_EXPORT const unsigned char GraffiTab_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraffiTab_iOS_SDK/PublicHeader.h>
+
+
