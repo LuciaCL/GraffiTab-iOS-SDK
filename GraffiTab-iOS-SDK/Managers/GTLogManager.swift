96beafc2cc938f5ce453742cde669891aec5ea79
Georgi Christov <georgi.christov89@gmail.com>
2016-06-08 01:03:46 +0100

    Improved logging.

--- GraffiTab-iOS-SDK/Managers/GTLogManager.swift
+++ GraffiTab-iOS-SDK/Managers/GTLogManager.swift
@@ -13,18 +13,22 @@ public class GTLogManager: NSObject {
 
     public class func setupApplicationLogger(logToConsole: Bool, logToDeviceLogs: Bool, logToFile: Bool, level: DDLogLevel) {
         if logToConsole {
-            DDLog.addLogger(DDTTYLogger.sharedInstance(), withLevel: level) // TTY = Xcode console
+            addLogger(DDTTYLogger.sharedInstance(), level: level) // TTY = Xcode console
         }
         
         if logToDeviceLogs {
-            DDLog.addLogger(DDASLLogger.sharedInstance(), withLevel: level) // ASL = Apple System Logs
+            addLogger(DDASLLogger.sharedInstance(), level: level) // ASL = Apple System Logs
         }
         
         if logToFile {
             let fileLogger: DDFileLogger = DDFileLogger() // File Logger
             fileLogger.rollingFrequency = 60*60*24  // 24 hours
             fileLogger.logFileManager.maximumNumberOfLogFiles = 7
-            DDLog.addLogger(fileLogger, withLevel: level)
+            addLogger(fileLogger, level: level)
         }
+    }
+    
+    public class func addLogger(logger: DDLogger, level: DDLogLevel) {
+        DDLog.addLogger(logger, withLevel: level)
     }
 }
