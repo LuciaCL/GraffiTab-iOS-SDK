7734acfb250f9b787135a9f6c45e902e26736320
Georgi Christov <georgi.christov89@gmail.com>
2016-07-07 11:06:58 +0100

    Added retry mechanism to asset processing endpoint.

--- GraffiTab-iOS-SDK/Tasks/Asset/GTAssetNetworkTask.swift
+++ GraffiTab-iOS-SDK/Tasks/Asset/GTAssetNetworkTask.swift
@@ -11,26 +11,35 @@ import ObjectMapper
 
 class GTAssetNetworkTask: GTNetworkTask {
 
+    var failAttempts = 5
+    
     func awaitAssetProcessingComplete(originalJSON: AnyObject, completion: (asset: GTAsset) -> Void) {
         let asset = Mapper<GTAsset>().map(originalJSON["asset"])
         
-        GTUtils.runWithDelay(1) {
-            GTLog.logDebug(GTLogConstants.Tag, message: "Checking asset state - \(asset?.guid!)", forceLog: false)
-            
-            GTAssetManager.getAssetState(asset!.guid!, successBlock: { (response) in
-                let polledAsset = response.object as! GTAsset
+        if failAttempts > 0 {
+            GTUtils.runWithDelay(1) {
+                GTLog.logDebug(GTLogConstants.Tag, message: "Checking asset state - \(asset?.guid!)", forceLog: false)
                 
-                if polledAsset.state == GTAssetState.COMPLETED {
-                    GTLog.logDebug(GTLogConstants.Tag, message: "Asset \(asset?.guid!) finished processing", forceLog: false)
-                    completion(asset: polledAsset)
-                }
-                else {
+                GTAssetManager.getAssetState(asset!.guid!, successBlock: { (response) in
+                    let polledAsset = response.object as! GTAsset
+                    
+                    if polledAsset.state == GTAssetState.COMPLETED {
+                        GTLog.logDebug(GTLogConstants.Tag, message: "Asset \(asset?.guid!) finished processing", forceLog: false)
+                        completion(asset: polledAsset)
+                    }
+                    else {
+                        self.awaitAssetProcessingComplete(originalJSON, completion: completion)
+                    }
+                }) { (response) in
+                    GTLog.logError(GTLogConstants.Tag, message: "Failed to poll for asset state - \(response.message)", forceLog: true)
+                    self.failAttempts -= 1
                     self.awaitAssetProcessingComplete(originalJSON, completion: completion)
                 }
-            }) { (response) in
-                GTLog.logError(GTLogConstants.Tag, message: "Failed to poll for asset state - \(response.message)", forceLog: true)
-                completion(asset: asset!)
             }
+        }
+        else {
+            GTLog.logError(GTLogConstants.Tag, message: "Failed to poll for asset state after \(failAttempts) tries. Aborting", forceLog: true)
+            completion(asset: asset!)
         }
     }
 }
