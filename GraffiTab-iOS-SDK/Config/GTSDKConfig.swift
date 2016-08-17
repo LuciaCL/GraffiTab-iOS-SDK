43a79b5ab2f084b5f16a9ee141b4c89ce09d9e86
Georgi Christov <georgi.christov89@gmail.com>
2016-08-17 10:06:24 +0100

    Minor formatting.

--- GraffiTab-iOS-SDK/Config/GTSDKConfig.swift
+++ GraffiTab-iOS-SDK/Config/GTSDKConfig.swift
@@ -34,10 +34,10 @@ public class GTSDKConfig: NSObject {
         GTLog.setup(isLog!, logToDeviceLogs: isLog!, logToFile: isLog!, level: config.logLevel!)
         
         GTLog.logInfo(GTLogConstants.Tag, message: "Set SDK configuration", forceLog: true)
-        GTLog.logDebug(GTLogConstants.Tag, message: "Domain: \(config.domain)", forceLog: false)
-        GTLog.logDebug(GTLogConstants.Tag, message: "Log: \(config.logEnabled)", forceLog: false)
-        GTLog.logDebug(GTLogConstants.Tag, message: "Log level: \(config.logLevel)", forceLog: false)
-        GTLog.logDebug(GTLogConstants.Tag, message: "HTTPS: \(config.httpsEnabled)", forceLog: false)
+        GTLog.logDebug(GTLogConstants.Tag, message: "Domain: \(config.domain!)", forceLog: false)
+        GTLog.logDebug(GTLogConstants.Tag, message: "Log: \(config.logEnabled!)", forceLog: false)
+        GTLog.logDebug(GTLogConstants.Tag, message: "Log level: \(config.logLevel!)", forceLog: false)
+        GTLog.logDebug(GTLogConstants.Tag, message: "HTTPS: \(config.httpsEnabled!)", forceLog: false)
         GTLog.logDebug(GTLogConstants.Tag, message: "API url: \(GTApiDomainConstants.ApiUrl)", forceLog: false)
     }
 }
